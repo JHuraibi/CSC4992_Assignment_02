90dbb92525074f77083eb91e36eff775e2a81e9b
JHuraibi <fh1328@wayne.edu>
2020-08-04 00:52:32 -0400

    Working

--- TestFileGenerator.py
+++ TestFileGenerator.py
@@ -2,21 +2,44 @@
 # Assignment 2
 # Question 1 Add-on
 
-first_names = {"Teresa", "Edith", "Lillie", "Billy", "John", "Grady", "Brittney",
-               "Sara", "Dario", "Jody", "Enrique", "Nestor", "Michelle"}
+import random
 
-last_names = {"Hinton", "McCoy", "Coffey", "Talent", "Smith", "Carlson", "Barry",
-              "Jones", "Harrell", "ODonnell", "Baldwin", "Strickland", "Henry"}
+first_names = ["Teresa", "Edith", "Lillie", "Billy", "John", "Grady", "Brittney",
+               "Sara", "Dario", "Jody", "Enrique", "Nestor", "Michelle"]
+
+last_names = ["Hinton", "McCoy", "Coffey", "Talent", "Smith", "Carlson", "Barry",
+              "Jones", "Harrell", "ODonnell", "Baldwin", "Strickland", "Henry"]
 
 
-def green_light():
-    return True
+def randomize_names():
+    random.shuffle(first_names)
+    random.shuffle(last_names)
 
 
 def write_to_file():
-    pass
+    file = open("input.txt", "w")
+
+    for i in range(13):
+        file.write(first_names[i])
+        file.write(" ")
+        file.write(last_names[i])
+        file.write(": ")
+
+        number_of_scores = random.randint(5, 10)                                    # How many random scores to generate
+
+        for j in range(number_of_scores):
+            score = random.randint(490, 1000)
+            score = score / 10.0
+
+            file.write(str(score))
+
+            if j < number_of_scores:
+                file.write(", ")                                                    # Dont print ", " for last score
+
+        file.write("\n")
+
 
 
 if __name__ == '__main__':
-    if green_light():
-        write_to_file()
+    randomize_names()
+    write_to_file()
