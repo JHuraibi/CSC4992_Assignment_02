8e7548097d3abfa7281c0cf215ce95881be75641
JHuraibi <fh1328@wayne.edu>
2020-08-04 00:32:50 -0400

    Skeleton

---
+++ TestFileGenerator.py
@@ -0,0 +1,22 @@
+# Author: Jamal Huraibi, fh1328
+# Assignment 2
+# Question 1 Add-on
+
+first_names = {"Teresa", "Edith", "Lillie", "Billy", "John", "Grady", "Brittney",
+               "Sara", "Dario", "Jody", "Enrique", "Nestor", "Michelle"}
+
+last_names = {"Hinton", "McCoy", "Coffey", "Talent", "Smith", "Carlson", "Barry",
+              "Jones", "Harrell", "ODonnell", "Baldwin", "Strickland", "Henry"}
+
+
+def green_light():
+    return True
+
+
+def write_to_file():
+    pass
+
+
+if __name__ == '__main__':
+    if green_light():
+        write_to_file()
